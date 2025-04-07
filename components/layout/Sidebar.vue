997a976559daf287433932ed751f65e5e8a51955
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-07 21:26:22 +0300

    Updated new component 'Sidebar' - Updated styles for template using 'tailwindcss'

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,7 +1,13 @@
 <template>
-  <aside class="sidebar">
-    <NuxtLink to="/">
-      <NuxtImg src="/logo.svg" alt="Logo" width="100px" height="auto" />
+  <aside class="px-5 py-8 bg-sidebar h-full w-full reletive">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg
+        src="/logo.svg"
+        alt="Logo"
+        width="100px"
+        height="auto"
+        class="mx-auto"
+      />
     </NuxtLink>
   </aside>
 </template>
