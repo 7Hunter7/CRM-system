584a0881eb7542fae304fd505502eebb9a5b9636
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-07 22:26:31 +0300

    Installed new module 'nuxtjs/google-fonts' and updated 'nuxt.config.ts' using it

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,25 @@
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   css: ["~/assets/css/main.css"],
   // Подключаем модули
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Lato: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
+
   shadcn: {
     prefix: "Ui",
     componentDir: "./components/ui",
