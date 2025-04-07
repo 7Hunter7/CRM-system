bec737de6931fa790c602e4f4cfb128e5602ccd7
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-07 20:39:42 +0300

    Inited Git.Created new layout 'default.vue'

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="default-layout">
+    <h1>Default Layout</h1>
+    <slot />
+  </section>
+</template>
+
+<script setup lang="ts">
+console.log("Loaded"); // Компонент создан
+</script>
+
+<style scoped></style>
