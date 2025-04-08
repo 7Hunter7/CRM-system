69ddd1fbe1f834d091ed95bb071a7ff259422f5f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-08 08:49:27 +0300

    Updated 'package.json' - Installes 'reka-ui'. Updated 'main.css' to 'tailwind.css' in 'assets/css'. Updated 'tailwind.config.js'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: false },
-  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
   // Подключаем модули
   modules: [
     "@nuxt/image",
