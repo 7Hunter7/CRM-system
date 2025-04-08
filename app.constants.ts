9ce92ec68befb1a6ed0266a0a68d35994f7ee8a0
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-08 11:36:05 +0300

    Updated file constants 'app.constants.ts'

--- app.constants.ts
+++ app.constants.ts
@@ -1,6 +1,6 @@
-export const APP_WRITE_ID = "67f4dc650031747fd198";
-export const DB_ID = "";
-export const COLLECTION_DEALS = "deals";
-export const COLLECTION_CUSTOMERS = "customers";
-export const COLLECTION_COMMENTS = "comments";
-export const STORAGE_ID = "storage";
+export const APP_WRITE_ID = "67f4dc650031747fd198"; // ID проекта Appwrite
+export const DB_ID = ""; // ID базы данных Appwrite
+export const COLLECTION_DEALS = "deals"; // Сделки
+export const COLLECTION_CUSTOMERS = "customers"; // Клиенты
+export const COLLECTION_COMMENTS = "comments"; // Комментарии
+export const STORAGE_ID = "storage"; // ID хранилища
