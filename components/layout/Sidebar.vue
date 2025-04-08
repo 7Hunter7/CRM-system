a98a83524e06d692915f738962cee89854825fde
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-08 10:23:51 +0300

    Updated 'package.json' - Installed '@iconify-json/line-md'. Updated 'Sidebar.vue' - Updated icon to 'line-md:logout'

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="px-5 py-8 bg-sidebar h-full w-full reletive">
+  <aside class="reletive px-5 py-8 bg-sidebar h-full w-full">
     <NuxtLink to="/" class="mb-10 block">
       <NuxtImg
         src="/logo.svg"
@@ -10,11 +10,7 @@
       />
     </NuxtLink>
     <button class="absolute top-2 right-3 transition-colors hover:text-primary">
-      <Icon
-        name="line-md:logout
-   "
-        size="20px"
-      />
+      <Icon name="line-md:logout" size="20px" />
     </button>
     <UiButton class="mb-5" var="primary" size="lg" to="/">Главная</UiButton>
   </aside>
