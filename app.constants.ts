056f5be829340be4ec01ed8da42f3ece0b26e875
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-08 19:52:32 +0300

    Updated 'app.constants.ts'

--- app.constants.ts
+++ app.constants.ts
@@ -1,5 +1,5 @@
 export const APP_WRITE_ID = "67f4dc650031747fd198"; // ID проекта Appwrite
-export const DB_ID = ""; // ID базы данных Appwrite
+export const DB_ID = "crm-database"; // ID базы данных Appwrite
 export const COLLECTION_DEALS = "deals"; // Сделки
 export const COLLECTION_CUSTOMERS = "customers"; // Клиенты
 export const COLLECTION_COMMENTS = "comments"; // Комментарии
