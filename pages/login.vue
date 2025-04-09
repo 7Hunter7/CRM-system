362cb6107ad7af73d3e6fb447675513937411c7a
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-09 07:33:57 +0300

    Upated new page 'pades/login.vue' - Added using 'UiInput' and 'UiButton'

--- pages/login.vue
+++ pages/login.vue
@@ -3,11 +3,45 @@
     <div class="rounded bg-sidebar w-1/4 p-5">
       <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
 
-      <form></form>
+      <form>
+        <UiInput class="mb-3" type="text" placeholder="Name" v-model="name" />
+        <UiInput
+          class="mb-3"
+          type="email"
+          placeholder="Email"
+          v-model="email"
+        />
+        <UiInput
+          class="mb-3"
+          type="password"
+          placeholder="Password"
+          v-model="password"
+        />
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button"> Login </UiButton>
+          <UiButton type="button" variant="secondary"> Register </UiButton>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+useHead({
+  title: "CRM System | Login",
+  meta: [
+    {
+      name: "description",
+      content: "Login page for the application",
+    },
+  ],
+});
+
+const name = ref<string>("");
+const email = ref<string>("");
+const password = ref<string>("");
+</script>
 
 <style scoped lang="scss"></style>
