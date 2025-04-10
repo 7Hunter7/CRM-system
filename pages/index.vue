21c64a861d9f409c73b046c2f57bcb2ac9b9c926
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-10 16:14:39 +0300

    Updated 'pages/index.vue' - Updated template using new UI-component 'Card'

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System</h1>
+    <UiCard class="mb-3" draggable="true">
+      <UiCardHeader role="button"> Card name </UiCardHeader>
+      <UiCardContent> Компания </UiCardContent>
+      <UiCardFooter> Date </UiCardFooter>
+    </UiCard>
   </div>
 </template>
 
