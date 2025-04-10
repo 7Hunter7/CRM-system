188d8588485ab8998a21d348dbc6a9c1ccac7d48
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-10 08:20:07 +0300

    Upated 'layouts/default.vue' - Added import 'account', 'useAuthStore', 'useIsLoadingStore' and 'useRouter' and using them in onMounted() for check user

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,30 @@
 </template>
 
 <script setup lang="ts">
-console.log("Loaded"); // Компонент создан и загружен
+import { account } from "@/utils/appwrite";
+import { useAuthStore, useIsLoadingStore } from "@/store/auth.store";
+import { useRouter } from "vue-router";
+
+const isLoadingStore = useIsLoadingStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(async () => {
+  try {
+    const user = await account.get();
+    if (user) {
+      // Сохраняем пользователя в store
+      authStore.setUser(user);
+    } else {
+      router.push("/login");
+    }
+  } catch (error) {
+    router.push("/login");
+    console.error("Error onmounted layout:", error);
+  } finally {
+    isLoadingStore.set(false);
+  }
+});
 </script>
 
 <style scoped>
