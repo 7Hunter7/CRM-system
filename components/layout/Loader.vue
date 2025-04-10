b95eada00f3d9a67e89342d48f6a74d5ad9bfd28
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-10 12:19:05 +0300

    Updated 'Loader.vue' - Updated template using inline styles to wrapper for 'NuxtImg'

--- components/layout/Loader.vue
+++ components/layout/Loader.vue
@@ -1,13 +1,5 @@
 <template>
-  <div class="wrapper">
+  <div class="bg-sidebar flex items-center justify-center w-screen h-screen">
     <NuxtImg src="/loader.svg" alt="Loader" />
   </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped lang="scss">
-.wrapper {
-  @apply bg-sidebar flex items-center justify-center w-screen h-screen;
-}
-</style>
