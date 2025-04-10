a0e20ec341e68565fe2e732cc70badf60750c865
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-10 08:51:09 +0300

    Upated 'Sidebar.vue' - Added new function 'logout' and updated template using it for logou button

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -9,13 +9,38 @@
         class="mx-auto"
       />
     </NuxtLink>
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button
+      class="absolute top-2 right-3 transition-colors hover:text-primary"
+      @click="logout"
+    >
       <Icon name="line-md:logout" size="20px" />
     </button>
     <LayoutMenu />
   </aside>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { account } from "@/utils/appwrite";
+import { useAuthStore, useIsLoadingStore } from "@/store/auth.store";
+import { useRouter } from "vue-router";
+
+const isLoadingStore = useIsLoadingStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+// Функция выхода из аккаунта
+const logout = async () => {
+  isLoadingStore.set(true);
+  try {
+    await account.deleteSession("current"); // Удаляем текущую сессию
+    authStore.clear(); // Очищаем данные пользователя из store
+    await router.push("/login");
+  } catch (error) {
+    console.error("Error logout:", error);
+  } finally {
+    isLoadingStore.set(false);
+  }
+};
+</script>
 
 <style scoped></style>
