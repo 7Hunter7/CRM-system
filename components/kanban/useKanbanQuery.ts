44c46de3860daf24f98355033b641a01eb311a15
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-11 08:58:14 +0300

    Updated 'useKanbanQuery.ts' - Updated 'select(data)' in function 'useKanbanQuery()' usinjg for/of to convert transactions and add them to appropriate columns

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -6,15 +6,33 @@ import type { ICard } from "./kanban.types";
 export function useKanbanQuery() {
   // Используем useQuery для получения данных из базы данных
   return useQuery({
-    queryKey: ["deals"],
-    queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+    queryKey: ["deals"], // Ключ запроса
+    queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS), // Функция для получения данных
 
+    // Функция для обработки данных после их получения
     select(data) {
       const newBoard = [...KANBAN_DATA];
       const deals = data.documents as unknown as ICard[];
 
       console.log("deals: ", deals);
       console.log("newBoard: ", newBoard);
+
+      // Перебираем сделки и добавляем их в соответствующие колонки
+      for (const deal of deals) {
+        const column = newBoard.find((col) => col.id === deal.status);
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: column.name,
+          });
+        }
+      }
+
+      return newBoard;
     },
   });
 }
