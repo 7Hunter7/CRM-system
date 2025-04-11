508b432ef7e3125b29431891cb11a0b12bb10fcf
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-11 07:51:32 +0300

    Updated 'useKanbanQuery.ts' - Updated function 'useKanbanQuery', added 'select(data)'

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -1,10 +1,20 @@
 import { useQuery } from "@tanstack/vue-query";
-import { COLLECTION_DEALS, DB_ID } from "~/app.constants";
+import { COLLECTION_DEALS, DB_ID } from "@/app.constants";
+import { KANBAN_DATA } from "./kanban.data";
+import type { ICard } from "./kanban.types";
 
 export function useKanbanQuery() {
   // Используем useQuery для получения данных из базы данных
   return useQuery({
     queryKey: ["deals"],
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+
+    select(data) {
+      const newBoard = [...KANBAN_DATA];
+      const deals = data.documents as unknown as ICard[];
+
+      console.log("deals: ", deals);
+      console.log("newBoard: ", newBoard);
+    },
   });
 }
