e9898bc20a315059f40a0bac03558350418bfec6
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-11 11:36:24 +0300

    Updated 'pages\index.vue' - Updated template with using 'v-for=(column, index) in data', added clsses

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System</h1>
     <div v-if="isLoading">Loading...</div>
-    <UiCard class="mb-3" draggable="true">
-      <UiCardHeader role="button"> Card name </UiCardHeader>
-      <UiCardContent> Компания </UiCardContent>
-      <UiCardFooter> Date </UiCardFooter>
-    </UiCard>
+    <div v-else>
+      <div class="grid grid-cols-5 gap-16">
+        <div v-for="(column, index) in data" key="column.id">
+          <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+            {{ column.name }}
+          </div>
+          <div>
+            <UiCard class="mb-3" draggable="true">
+              <UiCardHeader role="button"> Card name </UiCardHeader>
+              <UiCardContent> Компания </UiCardContent>
+              <UiCardFooter> Date </UiCardFooter>
+            </UiCard>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
