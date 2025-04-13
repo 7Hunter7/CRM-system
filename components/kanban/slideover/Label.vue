a3c3375b346a057701131c04e9f3d41141ccf9db
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-13 10:45:59 +0300

    Added folder 'components\kanban\slideover'. Created new component 'Label.vue' for Slideover

---
+++ components/kanban/slideover/Label.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mb-3">
+    <!-- Отображение label, если он передан в props -->
+    <label v-if="labelText" class="opacity-75 mb-0.5 block text-xs">
+      {{ labelText }}
+    </label>
+    <!-- Слот для вложенного содержимого -->
+    <div class="text-base"><slot /></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  labelText: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+</script>
+
+<script setup lang="ts"></script>
