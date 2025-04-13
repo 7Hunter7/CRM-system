732cd21aae96792530d9db3c70ff94842e07e35c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-13 11:30:08 +0300

    Created new component 'Slideover.vue' for Slideover using components 'KanbanSlideoverTop' and 'KanbanSledeoverComments'

---
+++ components/kanban/slideover/Slideover.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <USlideover v-model="isLocalOpen">
+      <UCard
+        class="flex flex-col flex-1 overflow-y-auto"
+        :ui="{
+          body: { base: 'flex-1' },
+          ring: '',
+          divide: 'divide-y divide-border',
+        }"
+      >
+        <template #header>
+          <KanbanSlideoverTop />
+        </template>
+        <KanbanSledeoverComments />
+      </UCard>
+    </USlideover>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useDealsSlideStore } from "@/store/deal-slide.store";
+
+const store = useDealsSlideStore();
+
+// Состояние открытия слайдера
+const isLocalOpen = computed({
+  get: () => store.isOpen,
+  set: (value) => {
+    store.isOpen = value;
+  },
+});
+</script>
