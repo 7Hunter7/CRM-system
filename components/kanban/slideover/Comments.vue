93b9bf661cbd6a2ca41026a5f7609ba1684a5d00
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-13 12:45:31 +0300

    Created 'components\kanban\slideover\Comments.vue' - Added new component using functions 'useCommentsByDeal' and 'useCreateComment'

--- components/kanban/slideover/Comments.vue
+++ components/kanban/slideover/Comments.vue
@@ -1,7 +1,18 @@
-<template></template>
+<template>
+  <UiInput
+    placeholder="Оставьте комментарий"
+    v-model="commentRef"
+    @keyup.enter="writeComment()"
+  ></UiInput>
+</template>
 
 <script setup lang="ts">
-import { useDealsSlideStore } from "@/store/deal-slide.store";
+import type { IDeal } from "@/types/deals.types";
+import { useCommentsByDeal } from "./useCommentsByDeal";
+import { useCreateComment } from "./useCreateComment";
 
-const store = useDealsSlideStore();
+const { data, refetch, isLoading } = useCommentsByDeal();
+const { commentRef, writeComment } = useCreateComment(refetch);
+
+const card = data as unknown as IDeal; // Типизация для AppWrite
 </script>
