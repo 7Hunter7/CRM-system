ea004f2c7a7244eb28ded1d3e1ef7858f5a3aaed
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-13 12:34:11 +0300

    Created 'components\kanban\slideover\useCreateComment.ts' - Created new function 'useCreateComment()' using refetch, useMutation and onSuccess. Added function 'writeComment()' using mutate()

---
+++ components/kanban/slideover/useCreateComment.ts
@@ -0,0 +1,32 @@
+import { useMutation } from "@tanstack/vue-query";
+import { v4 as uuid } from "uuid";
+import { DB_ID, COLLECTION_COMMENTS } from "@/app.constants";
+import { useDealsSlideStore } from "@/store/deal-slide.store";
+
+// Функция для создания коментария к сделке
+export function useCreateComment(refetch: () => void) {
+  const store = useDealsSlideStore();
+  const commentRef = ref<string>(""); // Ссылка на текст комментария
+
+  const { mutate } = useMutation({
+    mutationKey: ["add comment", commentRef.value],
+    mutationFn: () =>
+      DB.createDocument(DB_ID, COLLECTION_COMMENTS, uuid(), {
+        text: commentRef.value,
+        deal: store.card?.id,
+      }),
+
+    onSuccess: () => {
+      refetch(); // Обновляем данные после добавления комментария
+      commentRef.value = ""; // Очищаем поле комментария
+    },
+  });
+
+  // Функция для добавления комментария
+  const writeComment = () => {
+    if (!commentRef.value) return;
+    mutate(); // Вызываем мутацию для добавления комментария
+  };
+
+  return { writeComment, commentRef };
+}
