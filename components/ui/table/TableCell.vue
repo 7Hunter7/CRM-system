70ec8f7a70296eb3f2dc5f223cf320f1042a1812
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-15 10:12:27 +0300

    Updated 'components\ui\table\TableCell.vue' - Updated paddings from '2' to '4' for 'TableCell'

--- components/ui/table/TableCell.vue
+++ components/ui/table/TableCell.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/utils'
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/utils";
 
 const props = defineProps<{
-  class?: HTMLAttributes['class']
-}>()
+  class?: HTMLAttributes["class"];
+}>();
 </script>
 
 <template>
   <td
     :class="
       cn(
-        'p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5',
-        props.class,
+        'p-4 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-0.5',
+        props.class
       )
     "
   >
