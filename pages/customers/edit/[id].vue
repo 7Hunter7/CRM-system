493017ce7d82da2ac9c295eaa543c406997447f9
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-24 20:35:15 +0300

    Updated 'pages\customers\edit\[id].vue' - Added imports, useHead, customerId from route

--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -1,3 +1,25 @@
 <template></template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { v4 as uuidv4 } from "uuid";
+import { storage} from '@/utils/appwrite'
+import { useMutation, useQuery } from "@tanstack/vue-query";
+import { DB_ID, COLLECTION_CUSTOMERS, STORAGE_ID } from "@/app.constants";
+import type { ICustomer } from "@/types/deals.types";
+
+interface ICustomerFromState 
+  extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {}
+
+useHead({
+  title: "CRM System | Customers edit",
+  meta: [
+    {
+      name: "description",
+      content: "Customers edit page for the CRM System",
+    },
+  ],
+});
+
+const route = useRoute()
+const customerId = route.params.id as string
+</script>
