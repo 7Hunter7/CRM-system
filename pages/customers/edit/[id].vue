0605ad73bb69d3ff528feb67df72687c011be86b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-24 20:53:17 +0300

    Updated 'pages\customers\edit\[id].vue' - Added 'useForm' using interface 'ICustomerFromState', watch using 'setValues' and const using 'defineField'

--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -8,7 +8,7 @@ import { DB_ID, COLLECTION_CUSTOMERS, STORAGE_ID } from "@/app.constants";
 import type { ICustomer } from "@/types/deals.types";
 
 interface ICustomerFromState 
-  extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {}
+  extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {};
 
 useHead({
   title: "CRM System | Customers edit",
@@ -20,6 +20,28 @@ useHead({
   ],
 });
 
-const route = useRoute()
-const customerId = route.params.id as string
+const route = useRoute();
+const customerId = route.params.id as string;
+
+const { handleSubmit, defineField, setFieldValue, setValues, values } = useForm<ICustomerFromState>();
+
+const { data, isSuccess } = useQuery({
+  queryKey: ['get customer', customerId ],
+  queryFn: () => DB.getDocument(DB_ID, COLLECTION_CUSTOMERS, customerId)
+});
+
+watch(isSuccess, () => {
+  const initialData = data.value as unknown as ICustomerFromState
+
+  setValues({
+    name: initialData.name,
+    email: initialData.email,
+    avatar_url: initialData.avatar_url,
+    from_source: initialData.from_source || '',
+  })
+});
+
+const [name, nameAttrs] = defineField('name');
+const [email, emailAttrs] = defineField('email');
+const [fromSource, fromSourceAttrs] = defineField('from_source');
 </script>
